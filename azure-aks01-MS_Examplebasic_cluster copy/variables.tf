9e135d0864d8df24498075cf683a6ccb510167d8
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2019-11-20 23:22:50 -0800

    add basic aks

---
+++ azure-aks01-MS_Examplebasic_cluster copy/variables.tf
@@ -0,0 +1,45 @@
+variable "azure_subscription_id" {}
+variable "azure_client_id" {}
+variable "azure_client_secret" {}
+variable "azure_tenant_id" {}
+
+variable "client_id" {}
+variable "client_secret" {}
+
+variable "agent_count" {
+    default = 3
+}
+
+variable "ssh_public_key" {
+    default = "~/.ssh/id_rsa.pub"
+}
+
+variable "dns_prefix" {
+    default = "k8stest"
+}
+
+variable cluster_name {
+    default = "k8stest"
+}
+
+variable resource_group_name {
+    default = "azure-k8stest"
+}
+
+variable location {
+    default = "Central US"
+}
+
+variable log_analytics_workspace_name {
+    default = "testLogAnalyticsWorkspaceName"
+}
+
+# refer https://azure.microsoft.com/global-infrastructure/services/?products=monitor for log analytics available regions
+variable log_analytics_workspace_location {
+    default = "eastus"
+}
+
+# refer https://azure.microsoft.com/pricing/details/monitor/ for log analytics pricing
+variable log_analytics_workspace_sku {
+    default = "PerGB2018"
+}
