25b4c1898bc3fc4f742fe64101c00a908b61773f
lazywinadmin <fxcat@lazywinadmin.com>
2020-01-09 18:34:23 -0800

    add virtual network test

---
+++ azure-virtualnetwork_subnets/main.tf
@@ -0,0 +1,73 @@
+#############################################################################
+# VARIABLES
+#############################################################################
+
+variable "azure_subscription_id" {}
+variable "azure_client_id" {}
+variable "azure_client_secret" {}
+variable "azure_tenant_id" {}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type    = string
+  default = "eastus"
+}
+
+# Virtual Network
+variable "vnet_cidr_range" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+# Subnets
+variable "subnet_prefixes" {
+  type    = list(string)
+  default = ["10.0.0.0/24", "10.0.1.0/24"]
+}
+
+variable "subnet_names" {
+  type    = list(string)
+  default = ["web", "database"]
+}
+
+#############################################################################
+# PROVIDERS
+#############################################################################
+
+provider "azurerm" {
+  subscription_id = var.azure_subscription_id
+  client_id = var.azure_client_id
+  client_secret = var.azure_client_secret
+  tenant_id = var.azure_tenant_id
+}
+
+#############################################################################
+# RESOURCES
+#############################################################################
+
+module "vnet-main" {
+  source              = "azure/vnet/azurerm"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  vnet_name           = var.resource_group_name
+  address_space       = var.vnet_cidr_range
+  subnet_prefixes     = var.subnet_prefixes
+  subnet_names        = var.subnet_names
+  nsg_ids             = {}
+
+  tags = {
+    environment = "dev"
+    costcenter  = "it"
+
+  }
+}
+
+#############################################################################
+# OUTPUTS
+#############################################################################
+
+output "vnet_id" {
+  value = module.vnet-main.vnet_id
+}
---
+++ azure-virtualnetwork_subnets/readme.md
@@ -0,0 +1,16 @@
+# Terraform - Create a new Windows Server 2019 with local-exec provisionner
+
+## Step 3 - Terraform
+
+```text
+terraform init
+terraform plan -var-file "secrets.tfvars"
+terraform apply -var-file "secrets.tfvars" --auto-approve
+terraform destroy -var-file "secrets.tfvars" --auto-approve
+
+terraform init
+terraform plan -var-file "secrets.tfvars" -var resource_group_name=main-vnet -out vnet.tfplan
+terraform apply "vnet.tfplan"
+```
+
+
