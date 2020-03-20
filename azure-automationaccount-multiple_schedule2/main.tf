72f153d083d7de9787e75d0421f9dbb2bda78706
lazywinadmin <fxcat@lazywinadmin.com>
2020-03-19 18:51:26 -0700

    another example

---
+++ azure-automationaccount-multiple_schedule2/main.tf
@@ -0,0 +1,67 @@
+data "local_file" "example" {
+  filename = "./scripts/myscript.ps1"
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "test-aamultinonarm2"
+  location = "west us"
+}
+
+resource "azurerm_automation_account" "example" {
+  name                = "aamultinonarm2"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  sku_name = "Basic"
+
+  tags = {
+    environment = "development"
+  }
+}
+
+resource "azurerm_automation_runbook" "example" {
+  name                    = "FX-GetProcess"
+  location                = azurerm_resource_group.example.location
+  resource_group_name     = azurerm_resource_group.example.name
+  automation_account_name = azurerm_automation_account.example.name
+  log_verbose             = "true"
+  log_progress            = "true"
+  description             = "This is an example runbook"
+  runbook_type            = "PowerShell"
+  content = data.local_file.example.content
+  publish_content_link {
+    uri = "https://lazywinadmin.com"
+  }
+}
+
+variable "schedule_interval_minutes" {
+  default = "15"
+  description = "How often in minutes to run the snapshot runbook"
+}
+
+variable "schedulename" {
+    default = "hourly-shed"
+}
+
+resource "azurerm_automation_schedule" "example" {
+  count = 4
+
+  name                    = "${var.schedulename}-${count.index}"
+  resource_group_name     = azurerm_resource_group.example.name
+  automation_account_name = azurerm_automation_account.example.name
+  frequency               = "Hour"
+  timezone                = "Eastern Standard Time"
+  #start_time              = local.schedule_start_times[count.index]
+  start_time              = timeadd("${timestamp()}", "${format("%d", (var.schedule_interval_minutes * count.index) + var.schedule_interval_minutes)}m")
+  description             = "This is an example schedule"
+}
+
+
+resource "azurerm_automation_job_schedule" "example" {
+  count = 4
+
+  resource_group_name     = azurerm_resource_group.example.name
+  automation_account_name = azurerm_automation_account.example.name
+  schedule_name           = "${var.schedulename}-${count.index}"
+  runbook_name            = azurerm_automation_runbook.example.name
+}
